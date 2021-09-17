a75a7b7906971e8bb41952813bfb5764201681de
stever13 <slr200006@yahoo.com>
2021-09-16 22:15:17 -0700

    attempt 2 at programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -7,15 +7,15 @@
 ### a solution that was already created and hence not recalculate the solution.
 
 makeCacheMatrix <- function(x = matrix()) {
-  iv <- NULL
-  set <- function(y) {
-    x <<- y
-    iv <<- NULL
+  iv <- NULL            # creates a Null solution
+  set <- function(y) {  # sets the matrix passed to the makeCacheMatrix as the primary matrix
+    x <<- y             # puts x into the parent environment
+    iv <<- NULL         # puts null iv into the parent environment
   }
-  get <- function() x
-  setiv <- function(inv) iv <<- inv
-  getiv <- function() iv
-  list(set = set, get = get, 
+  get <- function() x   # function to retrieve matrix
+  setiv <- function(inv) iv <<- inv   # function to set the solved inverse matrix into the parent environment 
+  getiv <- function() iv              # retrieve the existing solution from the parent environment
+  list(set = set, get = get,          # creates list of functions so each function can easily be accessed name
        setiv = setiv, 
        getiv = getiv)
 }
@@ -27,13 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ### retrieve the solution.  If not already calculated, it will solve for the inverse matrix.
 
 cacheSolve <- function(x, ...) {
-  iv <- x$getiv()
-  if(!is.null(iv)) {
+  iv <- x$getiv()                 # retrieves inverse matrix (iv) from parent environment
+  if(!is.null(iv)) {              # test is a solution is alrady in memory.  If so, then retrieves the solution
     message('Getting Solution')
     return(iv)
   }
-  data <- x$get()
+  data <- x$get()                 # if there was no solution in memory then retrieve matrix and solve for inverse matrix
   iv <- solve(data)
-  x$setiv(iv)
+  x$setiv(iv)                     # stores solution in memory
   iv      ## Return a matrix that is the inverse of 'x'
 }
